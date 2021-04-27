6efcad7cf75c74288b4f771fda82ccd39f4041e8
Casiel <casield@gmail.com>
2021-04-26 23:51:16 -0500

    try guid

--- src/Simulation/Objects/PhyObject.cs
+++ src/Simulation/Objects/PhyObject.cs
@@ -95,14 +95,14 @@ namespace QuixPhysics
 
         public static string createUID()
         {
-            var bytes = new byte[5];
+            /*var bytes = new byte[5];
             var random = new Random();
             random.NextBytes(bytes);
             var idStr = (Math.Floor((double)(random.Next() * 25)) + 10).ToString() + "_";
             // add a timestamp in milliseconds (base 36 again) as the base
-            idStr += (Math.Floor((double)(random.Next() * 25)) + 10).ToString();
+            idStr += (Math.Floor((double)(random.Next() * 25)) + 10).ToString();*/
 
-            return idStr;
+            return Guid.NewGuid().ToString().GetHashCode().ToString("x"); ;
         }
 
         public virtual void Move(MoveMessage message)
